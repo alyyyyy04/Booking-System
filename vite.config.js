21dcd1ae744dc852954533856083d30eeaef4f71
alyyyyy04 <alyssajaneprak@gmail.com>
2026-03-10 20:57:22 +0800

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: [
+      "booking-system-production-039e.up.railway.app"
+    ]
+  }
 })
